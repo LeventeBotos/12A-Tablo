1fb6bbb49b6c4ab1c0fbf5e7fae26541c1de033c
Levente Botos <botos.levente2007@gmail.com>
2026-03-02 09:06:04 +0100

    Initial Commit

---
+++ 12A Tablo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  12A Tablo
+//
+//  Created by Levente Botos on 2026. 03. 02..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 12A Tablo/_2A_TabloApp.swift
@@ -0,0 +1,17 @@
+//
+//  _2A_TabloApp.swift
+//  12A Tablo
+//
+//  Created by Levente Botos on 2026. 03. 02..
+//
+
+import SwiftUI
+
+@main
+struct _2A_TabloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
